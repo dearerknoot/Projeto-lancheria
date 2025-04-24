7bcbcfd5a4e389594d25ffca63ffd59ea29647e0
dearerknoot <thiagocarlesso08@gmail.com>
2025-04-24 20:59:56 -0300

    commit

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 
+
 let itens = document.querySelectorAll('.item');
-let itensLista = []
+let produtos = document.getElementById('produtos');
+let itensLista = JSON.parse(localStorage.getItem('itensLista')) || [];
 itens.forEach(item =>{
    item.addEventListener('click',()=>{
       item.style.backgroundColor = 'red';
-      const numero = item.dataset.numero
+      const numero = item.dataset.numero;
       const nome = item.dataset.nome;
       const valor = item.dataset.valor;
       const desc = item.dataset.desc;
-      itensLista.push({nome,valor,desc})
-      console.log(nome)
+      itensLista.push({nome,valor,desc});
+      console.log(itensLista[numero].nome)
       console.log(itensLista)
-      alert(`adicionou: ${itensLista[numero].nome} Valor:${itensLista.valor}R$ descrição: ${itensLista.desc}`)
+      alert('Item adicionado ao seu carrinho');
+      localStorage.setItem('itensLista', JSON.stringify(itensLista));
    })
 })
+itensLista.forEach((item)=>{
+   let titulo = document.createElement('h1')
+   titulo.textContent = item.nome
+   console.log(item.nome)
+   produtos.appendChild(titulo)  
+})
+// localStorage.clear(itensLista);
