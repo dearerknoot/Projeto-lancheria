daf4e5474089867bc5fca8673ac52e5d8e262f06
dearerknoot <thiagocarlesso08@gmail.com>
2025-04-25 18:59:10 -0300

    inclusão do lista.html

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 let itens = document.querySelectorAll('.item');
 let produtos = document.getElementById('produtos');
+let valorPagar = document.getElementById('valorPagar');
 let itensLista = JSON.parse(localStorage.getItem('itensLista')) || [];
 itens.forEach(item =>{
    item.addEventListener('click',()=>{
@@ -18,9 +19,21 @@ itens.forEach(item =>{
    })
 })
 itensLista.forEach((item)=>{
-   let titulo = document.createElement('h1')
-   titulo.textContent = item.nome
-   console.log(item.nome)
-   produtos.appendChild(titulo)  
+   let produto = document.createElement('h1');
+   let botaoProduto = document.createElement('button');
+   botaoProduto.textContent = 'Remover';
+   produto.textContent = item.nome + " " + item.valor + " R$ ";
+   produtos.appendChild(produto);
+   produto.appendChild(botaoProduto);
+   botaoProduto.onclick = ()=>{
+      let index = itensLista.indexOf(item);
+      console.log(index)
+      if (index > -1) {
+         itensLista.splice(index, 1);
+         console.log(itensLista)
+         localStorage.clear(index);
+         produtos.removeChild(produto)
+       }
+   }
 })
 // localStorage.clear(itensLista);
