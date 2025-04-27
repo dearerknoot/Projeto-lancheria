a766cd4481e7e557c6b38aef97fc6ad0fe0cc717
dearerknoot <thiagocarlesso08@gmail.com>
2025-04-27 18:11:25 -0300

    cmommit

--- index.js
+++ index.js
@@ -2,21 +2,19 @@
 
 let itens = document.querySelectorAll('.item');
 let produtos = document.getElementById('produtos');
-let valorPagar = document.getElementById('valorPagar');
+let valorPagar = document.getElementById('valorPagar'); // div para mostrar a soma dos valores no lista.html
 let itensLista = JSON.parse(localStorage.getItem('itensLista')) || [];
 let alerta = document.getElementById('alerta');
-let valorTotal = 0;
 if(itensLista.length === 0){
    let divCarrinho = document.getElementById('carrinho');
    divCarrinho.style.pointerEvents = 'none';
-   alerta.textContent = "X"
+   alerta.textContent = "X";
 }
 itens.forEach(item =>{
    item.addEventListener('click',()=>{
       // const numero = item.dataset.numero;
       const nome = item.dataset.nome;
       const valor = item.dataset.valor;
-      // valorTotal += parseFloat(valor);
       const desc = item.dataset.desc;
       itensLista.push({nome,valor,desc});
       // console.log(itensLista[numero].nome)
@@ -33,15 +31,34 @@ itensLista.forEach((item)=>{
    produto.textContent = item.nome + " " + item.valor + " R$ ";
    produtos.appendChild(produto);
    produto.appendChild(botaoProduto);
+
+   let valorTotal = 0
+   for(let i = 0; i < itensLista.length;i++){ // for que percorrre o itensLista para adicionar o .valor para uma soma total
+      let valorFloat = parseFloat(itensLista[i].valor);
+      valorTotal += valorFloat;
+   }
+   valorPagar.textContent = valorTotal.toFixed(2) + " R$" //usei o to fixed para o valor ficar uma casa decimal a mais ex.: coca 7.5 para 7.50
+
    botaoProduto.onclick = ()=>{ // remove o item do array itenslista
       let index = itensLista.indexOf(item);
       console.log(index)
       if (index > -1) {
          itensLista.splice(index, 1);
-         console.log(itensLista)
+         console.log(itensLista);
          localStorage.setItem('itensLista', JSON.stringify(itensLista));
          produtos.removeChild(produto);
-         console.log(itensLista)
+         console.log(itensLista);
+         window.location.reload();
+       }
+       if(itensLista.length == 0){ // verificação pra quando o array itenslista estiver vazio, direcionar para a pagina principal
+         window.location.href = 'index.html';
        }
    }
 })
+
+let botaoEnviar = document.querySelector('.botao');
+botaoEnviar.onclick = ()=>{
+   alert('Pedido enviado com sucesso!');
+   localStorage.clear(itensLista)
+   window.location.href = 'produção.html';
+}
